470ab875db0d2aae671b96483c490ab736883e81
Elexandro Torres <elexandro.torres.ext@sascar.com.br>
2024-07-24 16:58:26 -0300

    initial project structure

---
+++ src/main/java/com/elexandrotorres/restaurantmenumanagement/RestaurantmenumanagementApplication.java
@@ -0,0 +1,13 @@
+package com.elexandrotorres.restaurantmenumanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantmenumanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantmenumanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elexandrotorres/restaurantmenumanagement/RestaurantmenumanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elexandrotorres.restaurantmenumanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantmenumanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
